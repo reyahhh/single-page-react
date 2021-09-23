3d4443b1e37b7172995849a7acfc0d84be942074
rere <reyiahornejas@gmail.com>
2021-09-24 03:47:31 +0800

    .

---
+++ src/Footer/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink,
+SocialMediaWrap, SocialMedia, SocialLogo, SocialIcons, SocialIconLink, WebsiteRights } from './FooterElements'
+
+const Footer = () => {
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Testimonials</FooterLink>
+                                <FooterLink to="/signin">Careers</FooterLink>
+                                <FooterLink to="/signin">Investors</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/"
+                        onClick={toggleHome}>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla &copy; {new Date().getFullYear()}
+                             &nbsp; All rights reserved.
+                        </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="//facebook.com" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="//instagram.com" target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="//youtube.com" target="_blank" aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href="//twitter.com" target="_blank" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="//linkedin.com" target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
---
+++ src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignIn from '../components/SignIn' 
+import ScrollToTop from '../components/SignIn/ScrollToTop'
+const SigninPage = () => {
+    return (
+        <div>
+           <ScrollToTop />
+           <SignIn />
+        </div>
+    )
+}
+
+export default SigninPage
